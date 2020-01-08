a56d2e067fb448ffd810a2ec4635aab1278d087b
Olayinka Otuniyi <olayinkaotuniyi@yahoo.co.uk>
2020-01-08 15:53:00 +0000

    Update __init__.py

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,21 +1,8 @@
 from flask import Flask
 from .src.views import src_blueprint
 from .build.views import build_blueprint
-#from flask_webpack import Webpack
-
-#webpack = Webpack()
-
 
 app = Flask(__name__)
-
-"""params = {
-    'DEBUG': True,
-    'WEBPACK_MANIFEST_PATH': './src/main/static/manifest.json',
-    "WEBPACK_ASSETS_URL": "webpack.common.js"
-}"""
-
-#app.config.update(params)
-#webpack.init_app(app)
 
 # register the blueprints
 app.register_blueprint(src_blueprint, url_prefix="/src")
