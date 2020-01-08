a57c51fe293946dd674081753e688611f1a96ce9
Olayinka Otuniyi <olayinkaotuniyi@yahoo.co.uk>
2020-01-08 15:55:12 +0000

    Add files via upload

--- templates/src/main/public/index.tsx
+++ templates/src/main/public/index.tsx
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom';
+import {hydrate} from 'react-dom';
 import '../static/css/index.css';
 import * as serviceWorker from '../static/serviceWorker';
-import routes from "./routes"; 
+import routes from "./routes";
 
-
-ReactDOM.render(routes, document.getElementById('root'));
+//ReactDOM.render(routes, document.getElementById('root'));
+hydrate(routes,  document.querySelector('#root'));
 serviceWorker.unregister();
-//export default routes;
+    //export default routes;
