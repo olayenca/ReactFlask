b25d9559ac47ca86f878001293905cb51e9ccea9
Olayinka Otuniyi <olayinkaotuniyi@yahoo.co.uk>
2020-01-09 10:59:57 +0000

    Update index.tsx

--- templates/src/main/public/index.tsx
+++ templates/src/main/public/index.tsx
@@ -4,5 +4,6 @@ import '../static/css/index.css';
 import * as serviceWorker from '../static/serviceWorker';
 import routes from "./routes";
 
-hydrate(routes, document.querySelector('#root'));
+console.log(process)
+hydrate(routes, document.getElementById('root'));
 serviceWorker.unregister();
