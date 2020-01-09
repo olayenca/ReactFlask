3a6106d2b4f0a388c1c1bcf51f1dd89bd2d11467
Olayinka Otuniyi <olayinkaotuniyi@yahoo.co.uk>
2020-01-09 10:59:36 +0000

    Update server.js

--- templates/src/main/public/server.js
+++ templates/src/main/public/server.js
@@ -3,8 +3,12 @@ import { renderToString } from "react-dom/server";
 import routes from "./routes";
 
 export default function render() {
-  let content = renderToString(<div>server-generated</div>);
+  let content_rendered = renderToString(React.createElement(routes));
+  console.log(content_rendered)
+  console.log(process)
   return {
     content
   };
 }
+
+//search for: react rendertostring from python
